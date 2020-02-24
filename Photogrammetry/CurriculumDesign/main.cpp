1f7182bb6534b4015db8d4cff111bdf9130e0ccf
xiake0o <lengfeng1453@163.com>
2020-02-24 19:36:34 +0800

    修改课程设计输出文件为xls格式

--- Photogrammetry/CurriculumDesign/main.cpp
+++ Photogrammetry/CurriculumDesign/main.cpp
@@ -9,7 +9,7 @@ int main() {
     project.setCameraFocal(70.50);//设置相机焦距
     project.setControlNumber(6);//设置控制点数
     project.setInputPath("../files/42.csv");//输入文件路径
-    project.setOutPath("../files/output.xlsx");//输出文件路径
+    project.setOutPath("../files/output.xls");//输出文件路径
     cout << "像片数：" << project.number_photo << endl;
     cout << "所有点：" << project.AllMarkPointsNumber << endl;
     cout << "所有图上点：" << project.AllPhotoPointsNumber << endl;
